e8cf7eb486ef1b2b7076b6153996646180345dda
Gerardo Nunez <gerardo.nunez@gmail.com>
2025-07-01 18:34:19 +0200

    test

--- data/watering_settings.py
+++ data/watering_settings.py
@@ -3,15 +3,15 @@ def get_online_settings():
     settings = dict()
     settings["BOOT_DURATION"] = 5 * 60       # 5 minutes at boot
     settings["SMALL_DURATION"] = 2 * 60      # 2 minutes
-    settings["LARGE_DURATION"] = 1 * 60      # 3 minutes
+    settings["LARGE_DURATION"] = 3 * 60      # 3 minutes
 
     # --- Watering Schedule ---
     # Small: 19h00  UTC = 21h00 France
     # Large: 19h10  UTC = 21h10 France
     settings["SMALL_HOUR"] = 21 - 2
     settings["SMALL_MIN"] = 0
-    settings["LARGE_HOUR"] = 17 - 2 
-    settings["LARGE_MIN"] = 41
+    settings["LARGE_HOUR"] = 21 - 2 
+    settings["LARGE_MIN"] = 10
 
     # --- Watering frequency ---
     settings["SMALL_PERIOD"] = 2    # One day watering, one day off
